df5743565093c2abf3eef42bbb33d970f251ec32
samarthar <153029993+samarthar@users.noreply.github.com>
2026-03-24 15:31:27 +0530

    Update myfile.java

--- myfile.java
+++ myfile.java
@@ -1,33 +1,6 @@
-3rd
+
 public class myfile {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-}
-4th
-    
-package example1.example1;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-public class example1 {
-
-	public static void main(String[] args) throws IOException,ParseException {
-		// TODO Auto-generated method stub
-JSONParser jsonparser=new JSONParser();
-FileReader filereader=new FileReader(".\\jsonfile\\example1.json");
-Object obj=jsonparser.parse(filereader);
-JSONObject empobj=(JSONObject) obj;
-String fname=(String) empobj.get("Firstname");
-String lname=(String) empobj.get("Lastname");
-System.out.println("Firstname:"+fname);
-System.out.println("Lastname:"+lname);
-
-	}
-
 }
